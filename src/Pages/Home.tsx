f8ff8dc5f1755f08144ad1a7454cec82d2e51edf
raypunzle <rayndrafattaahillah@gmail.com>
2024-07-31 09:15:41 +0700

    Update Home page

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -1,10 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Navbar from '../Component/Navbar';
-import Triangle from '../Media/triangle.png';
-import Ellipse from '../Media/ellipse.png'
+import Triangle from '../assets/Media/triangle.png';
+import Ellipse from '../assets/Media/ellipse.png';
 
 function Home() {
+  const cardData = [
+    {
+      title: 'Boards',
+      content: 'TaskFlow boards keep tasks organized and work moving forward. In a glance, see everything from "things to do" to "aww yeah we did it!"'
+    },
+    {
+      title: 'Lists',
+      content: 'The Different stages of task. Start as simple as To Do, Doing or Done-or build a workflow custom fit to your team\'s needs. There\'s no wrong way to TaskFlow.'
+    },
+    {
+      title: 'Cards',
+      content: 'Cards represent tasks and ideas and hold all the information to get the job done. As you make progress, move cards across lists to tshow their status.'
+    },
+    {
+      title: 'Report',
+      content: 'Summarize how active one is in taking tasks, then also count the number of tasks taken, and calculate the workload taken. This summary will be displayed automatically on Friday, or you can also set it to be a monthly report instead of a weekly one.'
+    }
+  ];
+
   return (
     <div className='min-h-screen bg-white font-inter'>
       <div className='relative z-10 flex flex-col min-h-screen'>
@@ -28,7 +47,7 @@ function Home() {
           <div className='absolute inset-0 z-0'>
             <img src={Ellipse} alt='Background' className='w-full h-46 object-cover -mt-32' />
           </div>
-          <div className='relative z-10 w-full md:w-2/3 lg:w-1/2 mb-8 text-center md:text-left'>
+          <div className='relative z-10 w-full md:w-2/3 lg:w-1/2 mb-20 text-center md:text-left'>
             <h1 className='text-2xl sm:text-3xl md:text-4xl text-black pt-16 sm:pt-24 md:pt-36 mb-4 sm:mb-6 md:mb-8'>
               A Powerhouse for efficiency.
             </h1>
@@ -36,10 +55,13 @@ function Home() {
               Easy, adaptable and effective. Boards, lists, cards and summary report provide a clear view of tasks and responsibilities
             </p>
           </div>
-          <div className='flex flex-row'>
-            <div>
-              
-            </div>
+          <div className='grid gap-4 w-2/5'>
+            {cardData.map((card, index) => (
+              <div key={index} className="bg-white border-l-8 border-sky-400 rounded-lg shadow-lg p-6 mb-4">
+                <h3 className="text-xl text-black font-semibold mb-2">{card.title}</h3>
+                <p className="text-black">{card.content}</p>
+              </div>
+            ))}
           </div>
         </main>
       </div>
